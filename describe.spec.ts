d5b0deb99448cd707e393f2747d85f5dfa349002
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-06-21 23:34:49 +0200

    feat: describe Private

---
+++ describe.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+
+import { describe as describeSclElement } from "./describe.js";
+
+const testScl = new DOMParser().parseFromString(
+  `<SCL
+      xmlns="http://www.iec.ch/61850/2003/SCL"
+      xmlns:sxy="http://www.iec.ch/61850/2003/SCLcoordinates"
+      xmlns:ens="http://somevalidURI"
+    >
+      <Private type="someType" desc="someDesc" sxy:x="10" ens:some="someOtherNameSpace">
+        <![CDATA[some comment]]>
+        <IED name="somePrivateIED"/>
+      </Private>
+      <ens:SomeNonSCLElement />
+    </SCL>`,
+  "application/xml"
+);
+
+const privateElement = testScl.querySelector("Private")!;
+const sclElement = testScl.querySelector("SCL")!;
+const SomeNonSCLElement = testScl.querySelector("SomeNonSCLElement")!;
+
+describe("Describe SCL elements function", () => {
+  it("returns undefined with missing describe function", () =>
+    expect(describeSclElement(SomeNonSCLElement)).to.be.undefined);
+
+  it("returns undefined with missing describe function", () =>
+    expect(describeSclElement(sclElement)).to.be.undefined);
+
+  it("returns outerHTML for SCL element Private ", () =>
+    expect(describeSclElement(privateElement)).to
+      .equal(`<Private xmlns="http://www.iec.ch/61850/2003/SCL" type="someType" desc="someDesc" xmlns:sxy="http://www.iec.ch/61850/2003/SCLcoordinates" sxy:x="10" xmlns:ens="http://somevalidURI" ens:some="someOtherNameSpace">
+        <![CDATA[some comment]]>
+        <IED name="somePrivateIED"/>
+      </Private>`));
+});

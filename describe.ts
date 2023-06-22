b5313d6ab0ef70b8f216a9e4f71a19c893d39e06
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-06-22 23:02:48 +0200

    feat: describe Text

--- describe.ts
+++ describe.ts
@@ -1,11 +1,13 @@
 import { Private, PrivateDescription } from "./describe/Private.js";
+import { Text, TextDescription } from "./describe/Text.js";
 
-export type Description = PrivateDescription;
+export type Description = PrivateDescription | TextDescription;
 
 const sclElementDescriptors: Partial<
   Record<string, (element: Element) => Description>
 > = {
   Private,
+  Text,
 };
 
 export function describe(element: Element): Description | undefined {
---
+++ describe/Text.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+
+import { Text } from "./Text.js";
+
+const textElement = new DOMParser()
+  .parseFromString(
+    `<SCL
+      xmlns="http://www.iec.ch/61850/2003/SCL"
+      xmlns:sxy="http://www.iec.ch/61850/2003/SCLcoordinates"
+      xmlns:ens="http://somevalidURI"
+    >
+      <Text desc="someDesc" sxy:x="10" ens:some="someOtherNameSpace">
+        <![CDATA[some comment]]>
+        Some more detailed description than the desc field
+      </Text>
+    </SCL>`,
+    "application/xml"
+  )
+  .querySelector("Text")!;
+
+describe("Description for SCL element Text", () => {
+  it("returns outerHTML", () =>
+    expect(Text(textElement)).to
+      .equal(`<Text xmlns="http://www.iec.ch/61850/2003/SCL" desc="someDesc" xmlns:sxy="http://www.iec.ch/61850/2003/SCLcoordinates" sxy:x="10" xmlns:ens="http://somevalidURI" ens:some="someOtherNameSpace">
+        <![CDATA[some comment]]>
+        Some more detailed description than the desc field
+      </Text>`));
+});

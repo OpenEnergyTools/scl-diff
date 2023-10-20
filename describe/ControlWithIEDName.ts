89609397f18ff74efa6c6d966ceaa191d816792b
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-10-21 00:03:35 +0200

    feat: describe GSEControl

---
+++ describe/ControlWithIEDName.ts
@@ -0,0 +1,80 @@
+import { ControlDescription, describeControl } from "./Control.js";
+
+function compareIEDNameDescription(a: IEDName, b: IEDName): number {
+  const stringifiedA = JSON.stringify(a);
+  const stringifiedB = JSON.stringify(b);
+
+  if (stringifiedA < stringifiedB) return -1;
+  if (stringifiedA > stringifiedB) return 1;
+  return 0;
+}
+
+type IEDName = {
+  /** IEDName attribute apRef*/
+  apRef?: string;
+  /** IEDName attribute ldInst*/
+  ldInst?: string;
+  /** IEDName attribute prefix*/
+  prefix?: string;
+  /** IEDName attribute lnClass*/
+  lnClass?: string;
+  /** IEDName attribute lnInst*/
+  lnInst?: string;
+  /** IEDName child text content */
+  val?: string;
+};
+
+function describeIEDName(element: Element): IEDName {
+  const iedName: IEDName = {};
+
+  const [apRef, ldInst, prefix, lnClass, lnInst] = [
+    "apRef",
+    "ldInst",
+    "prefix",
+    "lnClass",
+    "lnInst",
+  ].map((attr) => element.getAttribute(attr));
+
+  const val = element.textContent;
+
+  if (apRef) iedName.apRef = apRef;
+  if (ldInst) iedName.ldInst = ldInst;
+  if (prefix) iedName.prefix = prefix;
+  if (lnClass) iedName.lnClass = lnClass;
+  if (lnInst) iedName.lnInst = lnInst;
+  if (val) iedName.val = val;
+
+  return iedName;
+}
+
+export interface ControlWithIEDNameDescription extends ControlDescription {
+  /** ControlWithIEDName children IEDName */
+  iedNames: IEDName[];
+  /** ControlWithIEDName attribute confRev defaulted to 0 */
+  confRev?: number;
+}
+
+export function describeControlWithIEDName(
+  element: Element
+): ControlWithIEDNameDescription | undefined {
+  const controlDescription = describeControl(element);
+  if (!controlDescription) return;
+
+  const controlWithIEDNameDescription: ControlWithIEDNameDescription = {
+    ...controlDescription,
+    iedNames: [],
+  };
+
+  controlWithIEDNameDescription.iedNames.push(
+    ...Array.from(element.children)
+      .filter((child) => child.tagName === "IEDName")
+      .map((iedName) => describeIEDName(iedName))
+      .sort(compareIEDNameDescription)
+  );
+
+  const confRev = element.getAttribute("confRev");
+  if (confRev && !isNaN(parseInt(confRev, 10)))
+    controlWithIEDNameDescription.confRev = parseInt(confRev, 10);
+
+  return controlWithIEDNameDescription;
+}
---
+++ describe/GSEControl.ts
@@ -0,0 +1,44 @@
+import {
+  ControlWithIEDNameDescription,
+  describeControlWithIEDName,
+} from "./ControlWithIEDName.js";
+
+export interface GSEControlDescription extends ControlWithIEDNameDescription {
+  /** GSEControl attribute type defaulted to "GOOSE" */
+  type: "GOOSE" | "GSSE";
+  /** GSEControl attribute appId */
+  appID: string;
+  /** GSEControl attribute fixedOffs defaulted to false */
+  fixedOffs: boolean;
+  /** GSEControl attribute securityEnable defaulted to "None" */
+  securityEnable: "None" | "Signature" | "SignatureAndEncryption";
+  /**GSEControl child Protocol*/
+  protocol?: { mustUnderstand: true; val: "R-GOOSE" };
+}
+
+export function describeGSEControl(
+  element: Element
+): GSEControlDescription | undefined {
+  const controlWithTriggerOptDesc = describeControlWithIEDName(element);
+  if (!controlWithTriggerOptDesc) return;
+
+  const gseControlDescription: GSEControlDescription = {
+    ...controlWithTriggerOptDesc,
+    type: element.getAttribute("type") === "GSSE" ? "GSSE" : "GOOSE",
+    appID: element.getAttribute("appID") ?? "",
+    fixedOffs: element.getAttribute("fixedOffs") === "true" ? true : false,
+    securityEnable: element.getAttribute("securityEnable")
+      ? (element.getAttribute("securityEnable") as
+          | "Signature"
+          | "SignatureAndEncryption")
+      : "None",
+  };
+
+  const protocol = Array.from(element.children).find(
+    (child) => child.tagName === "Protocol"
+  );
+  if (protocol)
+    gseControlDescription.protocol = { mustUnderstand: true, val: "R-GOOSE" };
+
+  return gseControlDescription;
+}
